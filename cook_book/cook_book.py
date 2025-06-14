5c04fe924c4a410cb8e52dbfc1f94e6b5a310b81
Gleb <gleborel2@gmail.com>
2025-06-14 18:12:50 +0300

    refactoring

--- cook_book/cook_book.py
+++ cook_book/cook_book.py
@@ -1,38 +1,42 @@
-cook_book = dict()
+def parse_cook_book():
 
-with open("cook_book\\recipes.txt", encoding="utf-8") as file:
-    data = file.readlines()
+    with open("cook_book\\recipes.txt", encoding="utf-8") as file:
+        data = file.readlines()
+
+    cook_book = dict()
+    for line in data:
+
+        if not line.strip():
+            continue
+        if line.strip().isdigit():
+            continue
+        elif "|" in line.strip():
+            name, quantity, measure = [part.strip() for part in line.split("|")]
+            cook_book[key].append(
+                {"ingredient_name": name, "quantity": quantity, "measure": measure}
+            )
+        else:
+            cook_book.setdefault(line.strip(), [])
+            key = line.strip()
+
+    return cook_book
 
 
-for line in data:
+def show(cook_book):
+    for dish, ingredients in cook_book.items():
+        print(f"Блюдо {dish}")
+        for dict_ing in ingredients:
+            print(
+                f"\tНазвание ингредиента: {dict_ing['ingredient_name']:<15} | Количество : {dict_ing['quantity']:<10} | Единица измерения: {dict_ing['measure']:<5}"
+            )
 
-    if not line.strip():
-        continue
-
-    if line.strip().isdigit():
-        # cook_book[key].extend([dict() for _ in range(3)])
-        # оставил для теста обработки строки с кол-во инг.
-        continue
-    elif "|" in line.strip():
-        name, quantity, measure = [part.strip() for part in line.split("|")]
-        cook_book[key].append(
-            {"ingredient_name": name, "quantity": quantity, "measure": measure}
-        )
-    else:
-        cook_book.setdefault(line.strip(), [])
-        key = line.strip()
-
-for dish, ingredients in cook_book.items():
-    print(f"Блюдо {dish}")
-    for dict_ing in ingredients:
-        print(
-            f"\tНазвание ингредиента: {dict_ing['ingredient_name']:<15} | Количество : {dict_ing['quantity']:<10} | Единица измерения: {dict_ing['measure']:<5}"
-        )
-
-    print("\n")
+        print("\n")
 
 
 def get_shop_list_by_dishes(dishes, count_person):
+    cook_book = parse_cook_book()
+
+
     ingredients_for_dishes = dict()
 
     for dish in dishes:
@@ -43,7 +47,7 @@ def get_shop_list_by_dishes(dishes, count_person):
         for ingredient in cook_book[dish]:
             name = ingredient["ingredient_name"]
             measure = ingredient["measure"]
-            quantity = str(int(ingredient["quantity"]) * count_person)
+            quantity = int(ingredient["quantity"]) * count_person
 
             if name in ingredients_for_dishes:
                 ingredients_for_dishes[name]["quantity"] += quantity
@@ -56,7 +60,15 @@ def get_shop_list_by_dishes(dishes, count_person):
     return ingredients_for_dishes
 
 
-result = get_shop_list_by_dishes(["Запеченный картофель", "Омлет"], 2)
 
-for key, value in result.items():
-    print(f"{key:<10}: {value}")
+
+
+def main():
+    result = get_shop_list_by_dishes(["Запеченный картофель", "Омлет"], 2)
+
+    for key, value in result.items():
+        print(f"{key:<10}: {value}")
+
+
+if __name__ == "__main__":
+    main()
